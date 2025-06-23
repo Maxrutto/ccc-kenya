76b60f93c7bfee1209bbcaa3b27ff461eb0cd4e5
Maxrutto <ruttomaxmillan@gmail.com>
2025-06-23 10:47:35 +0300

    Web hosting 1

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -7,6 +7,12 @@ module.exports = {
         "primary-blue": "#e6f4ff",
         "primary-red": "#dc2626",
       },
+      fontFamily: {
+        'Montserrat': ['Montserrat', 'Montserrat-fallback', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', 'sans-serif'],
+        'Playfair_Display': ['"Playfair Display"', 'Playfair-fallback', 'Georgia', '"Times New Roman"', 'Times', 'serif'],
+        'Cinzel': ['Cinzel', 'Georgia', '"Times New Roman"', 'Times', 'serif'],
+        'Dancing_Script': ['"Dancing Script"', 'cursive', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'system-ui', 'sans-serif'],
+      },
     },
   },
   plugins: [],
